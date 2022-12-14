a75bea15c84a861c7eff5217cae071ed36533fc6
ShawonAshraf <shawon13@live.com>
2022-12-14 23:41:25 +0100

    cli now uses argparse

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-import sys
+import argparse
 
 from link import generate_colab_link, generate_colab_link_markdown
 
 if __name__ == "__main__":
-    allowed_options = {
-        "--str": generate_colab_link,
-        "--md": generate_colab_link_markdown
-    }
+    parser = argparse.ArgumentParser(
+        description="Generates a Google Link for your notebook in a Github repo")
 
-    option = sys.argv[1]
-    gh_link = sys.argv[2]
+    parser.add_argument("--gh",
+                        required=True,
+                        type=str, help="Link to your notebook in a public Github Repo")
 
-    colab_link = allowed_options[option](gh_link)
-    print(colab_link)
+    parser.add_argument("--string",
+                        dest="generate",
+                        help="the generated link will be a regular string or text which you can copy from your console",
+                        action="store_const",
+                        const=generate_colab_link)
+
+    parser.add_argument("--md",
+                        dest="generate",
+                        help="the generated link will be wrapped in a markdown block which you can use in readmes",
+                        action="store_const",
+                        const=generate_colab_link_markdown)
+
+    args = parser.parse_args()
+    print(args.generate(args.gh))
