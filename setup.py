b79ca30cd6d3ce2d41620cd962395e4a6101433d
ShawonAshraf <shawon13@live.com>
2022-12-14 23:54:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name='mytool',
+    version='0.0.1',
+    author='Shawon Ashraf',
+    author_email='shawon13@live.com',
+    license='MIT',
+    description='Generates Google Colab link for jupyter notebooks in public Github repos.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ShawonAshraf/github-to-colab-link',
+    py_modules=['link', 'cli'],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        colab-link-gen=cli
+    '''
+)
